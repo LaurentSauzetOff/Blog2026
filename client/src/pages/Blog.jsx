157e85af14ce3cbeae1dfde800ff54bba0d39c50
Laurent Sauzet <59488676+LaurentSauzetOff@users.noreply.github.com>
2026-03-23 16:03:05 +0100

    Feat/blog (#1)
    * feat: working on blog page
    
    * feat: Blog Page

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,9 +1,135 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import { assets, blog_data, comments_data } from "../assets/assets";
+import Moment from "moment";
+import Footer from "./../components/Footer";
+import Loader from "../components/Loader";
 
 const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
-}
+  const { id } = useParams();
+  const [data, setData] = useState(null);
+  const [comments, setComments] = useState([]);
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
 
-export default Blog
+  const fetchBlogData = async () => {
+    const data = blog_data.find((item) => item._id === id);
+    setData(data);
+  };
+
+  const fetchComments = async () => {
+    setComments(comments_data);
+  };
+
+  const addComment = async (e) => {
+    e.preventDefault;
+  };
+
+  useEffect(() => {
+    fetchBlogData();
+    fetchComments();
+  }, []);
+  return data ? (
+    <div className="relative">
+      <img
+        src={assets.gradientBackground}
+        className="absolute -top-50 -z-1 opacity-50"
+        alt=""
+      />
+      <Navbar />
+      <div className="text-center mt-20 text-gray-600">
+        <p className="text-primary py-4 font-medium">
+          Publié le {Moment(data.CreatedAt).format("DD/MMMM/YYYY")}
+        </p>
+        <h1 className="text-2xl sm:text-5xl font-semiboldmax-w-2xl mx-auto">
+          {data.title}
+        </h1>
+        <h2 className="my-5 max-w-lg truncate mx-auto">{data.subtitle}</h2>
+        <p className="inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary">
+          Laurent Sauzet
+        </p>
+      </div>
+      <div className="mx-5 max-w-5xl md:mx-auto my-10 mt-6">
+        <img src={data.image} alt="" className="rounded-3xl mb-5" />
+        <div
+          className="rich-text max-w-3xl mx-auto"
+          dangerouslySetInnerHTML={{ __html: data.description }}
+        ></div>
+
+        <div className="mt-14 mb-10 max-w-3xl mx-auto">
+          <p className="font-semibold mb-4">Commentaires ({comments.length})</p>
+          <div className="flex flex-col gap-4">
+            {comments.map((item, index) => (
+              <div
+                key={index}
+                className="relative bg-primary/2 border border-primary/5 max-w-xl p-4 rounded text-gray-600"
+              >
+                <div className="flex items-center gap-2 mb-2">
+                  <img src={assets.user_icon} className="w-6" alt="" />
+                  <p className="font-medium">{item.name}</p>
+                </div>
+                <p className="text-sm max-w-md ml-8">{item.content}</p>
+                <div className="absolute right-4 bottom-3 flex items-center gap-2 text-xs">
+                  {Moment(item.CreatedAt).fromNow()}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="max-w-3xl mx-auto">
+          <p className="font-semibold mb-4"></p>
+          <form
+            onSubmit={addComment}
+            className="flex flex-col items-start gap-4 max-w-lg"
+          >
+            <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              type="text"
+              placeholder="Nom"
+              required
+              className="w-full p-2 border border-gray-300 rounded outline-none"
+            />
+            <textarea
+              onChange={(e) => setContent(e.target.value)}
+              value={content}
+              placeholder="Votre commentaire"
+              className="w-full
+              p-2
+              border
+              border-gray-300
+              rounded
+              outline-none
+              h-48"
+            ></textarea>
+            <button
+              type="submit"
+              className="bg-primary text-white rounded p-2 px-8 hover:scale-102 transition-all cursor-pointer"
+            >
+              Soumettre
+            </button>
+          </form>
+        </div>
+
+        <div className="my-24 max-w-3xl mx-auto">
+          <p className="font-semibold my-4">
+            Partager cet article sur les réseaux sociaux
+          </p>
+          <div className="flex">
+            <img src={assets.facebook_icon} width={50} alt="Facebook" />
+            <img src={assets.twitter_icon} width={50} alt="Twitter" />
+            <img src={assets.linkedin_icon} width={50} alt="LinkedIn" />
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  ) : (
+    <Loader />
+  );
+};
+
+export default Blog;
