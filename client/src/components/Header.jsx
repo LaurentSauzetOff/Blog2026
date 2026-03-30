11d789e385057ecf1e2d28805ec3675b4adb4ee6
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2026-03-30 17:21:39 +0200

    feat: - setup axios instance and auth logic in AppContext ; - implement full login/logout flow and blog fetching logic ; - implement blog listing UI and conditional navigation.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import { assets } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
 
 const Header = () => {
+  const { setInput, input } = useAppContext();
+  const inputRef = useRef();
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    setInput(inputRef.current.value);
+  };
+
+  const onClear = () => {
+    setInput("");
+    inputRef.current.value = "";
+  };
+
   return (
     <div className="mx-8 sm:mx-16 xl:mx-24 relative">
       <div className="text-center mt-20 mb-8">
@@ -12,7 +26,11 @@ const Header = () => {
 
         <h1 className="text-3xl sm:text-6xl font-semibold sm:leading-16 text-foreground">
           LE blog sur la culture <br />{" "}
-          <span className="text-primary" /* style={{ color: "#00FF00" }} */> pop </span> !
+          <span className="text-primary" /* style={{ color: "#00FF00" }} */>
+            {" "}
+            pop{" "}
+          </span>{" "}
+          !
         </h1>
 
         <p className="my-6 sm:my-8 max-w-2xl m-auto max-sm:text-xs text-foreground">
@@ -21,10 +39,14 @@ const Header = () => {
           tendances et de découvertes avec nous !
         </p>
 
-        <form className="flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 bg-white rounded overflow-hidden">
+        <form
+          onSubmit={onSubmitHandler}
+          className="flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 bg-white rounded overflow-hidden"
+        >
           <input
+            ref={inputRef}
             type="text"
-            className="w-full pl-4 outline-none"
+            className="w-full pl-4 outline-none text-dark-text"
             placeholder="Rechercher un article..."
             required
           />
@@ -35,6 +57,16 @@ const Header = () => {
             Rechercher
           </button>
         </form>
+      </div>
+      <div className="text-center">
+        {input && (
+          <button
+            onClick={onClear}
+            className="border font-light text-xs py-1 px-3 rounded-sm shadow-custom-sm cursor-pointer"
+          >
+            Réinitialiser la recherche
+          </button>
+        )}
       </div>
 
       <img
