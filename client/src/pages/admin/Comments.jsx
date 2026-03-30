7fc0e9d72519a4bf24561c79e05be993ea4d5426
Laurent Sauzet <59488676+LaurentSauzetOff@users.noreply.github.com>
2026-03-30 18:50:16 +0200

    Feat/frontend api connection

--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -1,13 +1,22 @@
 import { comments_data } from "assets/assets";
 import CommentTableItem from "components/admin/CommentTableItem";
+import { useAppContext } from "context/AppContext";
 import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 const Comments = () => {
+  const { axios } = useAppContext();
+
   const [comments, setComments] = useState([]);
   const [filter, setFilter] = useState("not approved");
 
   const fetchComments = async () => {
-    setComments(comments_data);
+    try {
+      const { data } = await axios.get("/api/admin/comments");
+      data.success ? setComments(data.comments) : toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   useEffect(() => {
@@ -15,26 +24,26 @@ const Comments = () => {
   }, []);
 
   return (
-    <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50">
+    <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16">
       <div className="flex justify-between items-center max-w-3xl">
         <h1>Commentaires</h1>
         <div className="flex gap-3">
           <button
             onClick={() => setFilter("Approved")}
-            className="{`shadow-custom-sm border rounded-full px-4 py-1 curssor-pointer text-xs ${filter === 'Approved' ? 'text-primary' : 'text-foreground-700'}`}"
+            className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${filter === 'Approved' ? 'text-primary' : 'text-foreground-700'}`}
           >
             Validé
           </button>
           <button
             onClick={() => setFilter("Not Approved")}
-            className="{`shadow-custom-sm border rounded-full px-4 py-1 curssor-pointer text-xs ${filter === 'Not Approved' ? 'text-primary' : 'text-foreground-700'}`}"
+            className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${filter === 'Not Approved' ? 'text-primary' : 'text-foreground-700'}`}
           >
             Non validé
           </button>
         </div>
       </div>
 
-      <div className="relative h-4/5 mt-4 max-w-4xl overflow-x-auto shadow rounded-lg scrollbar-hide bg-white">
+      <div className="relative h-4/5 mt-4 max-w-4xl overflow-x-auto shadow rounded-lg scrollbar-hide">
         <table className="w-full text-sm text-foreground-500">
           <thead className="text-xs text-foreground-700 text-left uppercase">
             <tr>
@@ -50,10 +59,18 @@ const Comments = () => {
             </tr>
           </thead>
           <tbody>
-            {comments.filter((comment) => {
-              if (filter === "Approved") return comment.isApproved === true;
-              return comment.isApproved === false;
-            }).map((comment) => <CommentTableItem key={comment._id} comment={comment} fetchComments={fetchComments}/>)}
+            {comments
+              .filter((comment) => {
+                if (filter === "Approved") return comment.isApproved === true;
+                return comment.isApproved === false;
+              })
+              .map((comment) => (
+                <CommentTableItem
+                  key={comment._id}
+                  comment={comment}
+                  fetchComments={fetchComments}
+                />
+              ))}
           </tbody>
         </table>
       </div>
--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,8 +1,12 @@
 import { assets, dashboard_data } from "assets/assets";
 import BlogTableItem from "components/admin/BlogTableItem";
+import { useAppContext } from "context/AppContext";
 import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 const Dashboard = () => {
+  const { axios } = useAppContext();
+
   const [dashboardData, setDashboardData] = useState({
     blogs: 0,
     comments: 0,
@@ -11,7 +15,14 @@ const Dashboard = () => {
   });
 
   const fetchDashboard = async () => {
-    setDashboardData(dashboard_data);
+    try {
+      const { data } = await axios.get("/api/admin/dashboard");
+      data.success
+        ? setDashboardData(data.dashboardData)
+        : toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   useEffect(() => {
@@ -19,48 +30,48 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <div className="flex-1 p-4 md:p-10 blg-blue-50/50">
+    <div className="flex-1 p-4 md:p-10 ">
       <div className="flex flex-wrap gap-4">
-        <div className="flex items-center gap-4 bg-white p-4 min-w-58 rounded-shadow cursor-pointer hover:scale-105 transition-all">
-          <img src={assets.dashboard_icon_1} alt="" />
+        <div className="flex items-center gap-4 p-4  min-w-58 rounded-shadow cursor-pointer hover:scale-105 transition-all">
+          <img src={assets.dashboard_icon_1} alt="" className="bg-background"/>
           <div>
-            <p className="text-xl font-semibold text-dark-text">
+            <p className="text-xl font-semibold text-white">
               {dashboardData.blogs}
             </p>
-            <p className="text-dark-text font-light">Articles</p>
+            <p className="text-white font-light">Articles</p>
           </div>
         </div>
 
-        <div className="flex items-center gap-4 bg-white p-4 min-w-58 rounded-shadow curssor-pointer hover:scale-105 transition-all">
+        <div className="flex items-center gap-4 p-4 min-w-58 rounded-shadow curssor-pointer hover:scale-105 transition-all">
           <img src={assets.dashboard_icon_2} alt="" />
           <div>
-            <p className="text-xl font-semibold text-dark-text">
+            <p className="text-xl font-semibold text-white">
               {dashboardData.comments}
             </p>
-            <p className="text-dark-text font-light">Commentaires</p>
+            <p className="text-white font-light">Commentaires</p>
           </div>
         </div>
 
-        <div className="flex items-center gap-4 bg-white p-4 min-w-58 rounded-shadow curssor-pointer hover:scale-105 transition-all">
+        <div className="flex items-center gap-4 p-4 min-w-58 rounded-shadow curssor-pointer hover:scale-105 transition-all">
           <img src={assets.dashboard_icon_3} alt="" />
           <div>
-            <p className="text-xl font-semibold text-dark-text">
+            <p className="text-xl font-semibold text-white">
               {dashboardData.drafts}
             </p>
-            <p className="text-dark-text font-light">Brouillons</p>
+            <p className="text-white font-light">Brouillons</p>
           </div>
         </div>
       </div>
 
       <div>
-        <div className="flex items-center gap-3 m-4 mt-6 text-dark-text">
+        <div className="flex items-center gap-3 m-4 mt-6 text-white">
           <img src={assets.dashboard_icon_4} alt="" />
           <p>Derniers articles</p>
         </div>
 
-        <div className="relative max-w-4xl overflow-x-auto shadow rounded-lg scrollbar-hide bg-white">
-          <table className="w-full text-sm text-dark-text">
-            <thead className="text-xs text-dark-text text-left uppercase">
+        <div className="relative max-w-4xl overflow-x-auto shadow rounded-lg scrollbar-hide">
+          <table className="w-full text-sm text-white">
+            <thead className="text-xs text-white text-left uppercase">
               <tr>
                 <th scope="col" className="px-2 py-4 xl:px-6">
                   #
