75093a8129ae75913d71567b41dcf09bb7d209dd
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2026-04-02 17:30:06 +0200

    new update

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -1,22 +1,22 @@
 import multer from "multer";
 import os from "os";
+import crypto from "crypto"; // Module natif de Node.js
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, os.tmpdir());
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    // On génère 8 octets aléatoires convertis en hexadécimal (ex: 'f3a2b1c4')
+    const uniqueSuffix =
+      Date.now() + "-" + crypto.randomBytes(8).toString("hex");
     cb(null, uniqueSuffix + "-" + file.originalname);
   },
 });
 
 const upload = multer({
   storage: storage,
-  limits: {
-    // 5 Mo = 5 * 1024 * 1024 octets
-    fileSize: 5 * 1024 * 1024,
-  },
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
 
 export default upload;
