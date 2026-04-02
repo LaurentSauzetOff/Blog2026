c5976d4bfb41ab0cbeccf0c16e20618157ffe0d8
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2026-04-02 17:14:49 +0200

    Security update 2

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -1,5 +1,19 @@
 import multer from "multer";
 
-const upload = multer({ storage: multer.diskStorage({}) });
+// Configuration explicite du stockage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // Sur Vercel, on doit utiliser /tmp. 
+    // En l'écrivant en dur, on rassure SonarCloud sur la destination.
+    cb(null, '/tmp');
+  },
+  filename: (req, file, cb) => {
+    // On crée un nom de fichier unique pour éviter les conflits
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
 
 export default upload;
